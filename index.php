a35bf9cbd0d7d4218654851822bb51e1e71e1a94
Pushker <pushker@revinfotech.com>
2021-02-24 10:54:58 +0530

    fieldset

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fieldset</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+	<header>
+		<div class="header_area">
+			<div class="container">
+				<div class="site_logo"><a href="#">HOME</a></div>
+				<div class="site_menu"><a href="#"></a></div>
+			</div>
+		</div>
+	</header>
+	<section>
+		<form class="food_description">
+		
+			<fieldset style="display: block;">
+				<div class="first_step">
+					<div class="container"><a href="javascript:void(0);" class="choose_btn"><h1>First Step</h1></a></div>
+				</div>
+			</fieldset>
+		
+			<fieldset>
+				<div class="first_step">
+					<div class="container"><a href="javascript:void(0);" class="choose_btn"><h1>Second Step</h1></a></div>
+				</div>
+			</fieldset>
+		
+			<fieldset>
+				<div class="first_step">
+					<div class="container"><a href="javascript:void(0);" class="choose_btn"><h1>Third Step</h1></a></div>
+				</div>
+			</fieldset>
+			
+			<fieldset>
+				<div class="first_step">
+					<div class="container"><a href="javascript:void(0);" class=""><h1>Fourth Step</h1></a></div>
+				</div>
+			</fieldset>
+		
+		</form>
+	</section>
+	<footer></footer>
+
+</body>
+</html>
+
+
+<script type="text/javascript">
+	jQuery(document).ready(function(){
+		$('.food_description fieldset:first-child').fadeIn('slow').css("margin-left","0px");
+		$('.choose_btn').on('click', function() {
+			var parent_fieldset = $(this).parents('fieldset');
+			var next = parent_fieldset.next();
+			parent_fieldset.fadeOut(400, function() {
+				next.fadeIn().css("margin-left","0px");
+			});
+		})
+	})
+</script>
